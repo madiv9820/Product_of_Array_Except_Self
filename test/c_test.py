59ee5d80532ce38c74bb0042db1a35e3d7f4d114
Manish Mathur <madiv@Manishs-MacBook-Pro.local>
2026-01-27 02:58:45 +0530

    🧪 Add timeout-enforced unit tests for Python, C and C++ solutions  - Adds unittest-based test suites for both Python, C and C++ implementations  - Ensures identical test cases across languages for consistency  - Applies strict execution timeouts to detect inefficient logic  - Helps validate correctness, performance, and parity between implementations

---
+++ test/c_test.py
@@ -0,0 +1,26 @@
+import unittest
+from timeout_decorator import timeout
+from source.c.solution import cSolution
+
+class test_c_Solution(unittest.TestCase):
+    def setUp(self):
+        self.__testcases = (
+            ([1,2,3,4], [24,12,8,6]),
+            ([-1,1,0,-3,3], [0,0,9,0,0]),
+            ([5,10], [10,5]),
+            ([-2,-3,-4], [12,8,6]),
+            ([1] * 10**5, [1] * 10**5),
+            ([0,1,2,0], [0,0,0,0]),
+            ([30,-30,1,-1], [30,-30,-900,900])
+        )
+        self.__solution = cSolution()
+        return super().setUp()
+    
+    @timeout(2)
+    def test(self):
+        for nums, expectedOutput in self.__testcases:
+            with self.subTest(nums):
+                actualOutput = self.__solution.c_productExceptSelf(nums)
+                self.assertEqual(actualOutput, expectedOutput)
+
+if __name__ == '__main__': unittest.main()
